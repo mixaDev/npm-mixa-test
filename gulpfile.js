f5fc165deebeec933d0a50de06c71f3134d1a9af
Михайло Голубовський <lamerumiata@gmail.com>
2018-10-19 18:41:16 +0300

    create project

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp     = require('gulp');
+var del      = require('del');
+
+
+gulp.task('copy', function () {
+    return gulp.src('src/*.js')
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function(cb) {
+    del.sync('dist');
+    cb();
+});
+
+gulp.task('default',
+  gulp.series(
+    'clean',
+    'copy'
+  )
+);
