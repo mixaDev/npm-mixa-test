19261b54d3a6fbbf5df10caf389eb9ffbc98b8c4
Михайло Голубовський <lamerumiata@gmail.com>
2019-07-15 14:16:24 +0300

    refactor

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,19 +1,19 @@
-	#!/bin/bash
-	set -e
-	
-
-	if [[ -z $1 ]]; then
-	  echo "Enter new version: "
-	  read -r VERSION
-	else
-	  VERSION=$1
-	fi
+#!/bin/bash
+set -e
 
 
-	read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-	  echo "Releasing $VERSION ..."
+if [[ -z $1 ]]; then
+  echo "Enter new version: "
+  read -r VERSION
+else
+  VERSION=$1
+fi
+
+
+read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Releasing $VERSION ..."
 
 
 #	  if [[ -z $SKIP_TESTS ]]; then
@@ -22,23 +22,23 @@
 #	  fi
 
 
-	  # build
-	  VERSION=$VERSION npm run build
+  # build
+  VERSION=$VERSION npm run build
 
 
-	  git add .
-	  git diff --quiet && git diff --staged --quiet || git commit -am "build: build $VERSION"
+  git add .
+  git diff --quiet && git diff --staged --quiet || git commit -am "build: build $VERSION"
 
 
-	  # tag version
-	  npm version "$VERSION" --message "build: release $VERSION"
+  # tag version
+  npm version "$VERSION" --message "build: release $VERSION"
 
 
-	  # publish
-	  git push origin refs/tags/v"$VERSION"
-	  git push
+  # publish
+  git push origin refs/tags/v"$VERSION"
+  git push
 
 
-	  echo -e "\033[1;92m You are ready to publish!"
-	  echo -e "\033[1;95m npm publish"
-	fi
+  echo -e "\033[1;92m You are ready to publish!"
+  echo -e "\033[1;95m npm publish"
+fi
